f50e4adfcea2c7abb9863de53091edf9cf017df7
xli <Xiaotian.Li@lapisit.com.au>
2024-01-02 15:31:03 +1100

    update backend blogs related api

--- amplify/backend/function/blogsLambda/src/app.js
+++ amplify/backend/function/blogsLambda/src/app.js
@@ -7,86 +7,151 @@ See the License for the specific language governing permissions and limitations 
 */
 
 
-
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const express = require('express');
+const bodyParser = require('body-parser');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
 // declare a new express app
-const app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
 });
 
+// database
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  ScanCommand,
+  UpdateCommand
+} from '@aws-sdk/lib-dynamodb';
+
+const tableName = 'demo-blog-table';
+
+const client = new DynamoDBClient();
+
+const dynamo = DynamoDBDocumentClient.from(client);
 
 /**********************
  * Example get method *
  **********************/
 
-app.get('/blogs', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+app.get('/blogs', async function(req, res) {
+  const response = await dynamo.send(
+    new ScanCommand({
+      TableName: tableName
+    })
+  );
+  res.json({ success: 'get call succeed!', url: req.url, data: response.Items });
 });
 
-app.get('/blogs/*', function(req, res) {
+app.get('/blogs/:id', async function(req, res) {
+  const response = await dynamo.send(
+    new GetCommand({
+      TableName: tableName,
+      Key: {
+        id: req.params.id
+      }
+    })
+  );
+
+  res.json({ success: 'get call succeed!', url: req.url, data: response.Item });
+});
+
+
+/****************************
+ * Example post method *
+ ****************************/
+
+app.post('/blogs', async function(req, res) {
   // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  const requestJson = JSON.parse(req.body);
+  await dynamo.send(
+    new PutCommand({
+      TableName: tableName,
+      Item: {
+        id: requestJson.id,
+        title: requestJson.title,
+        content: requestJson.content,
+        description: requestJson.description,
+        author: requestJson.author,
+        createdTime: requestJson.createdTime,
+        updatedTime: requestJson.updatedTime,
+        location: {
+          lat: requestJson.location.lat,
+          lng: requestJson.location.lng,
+          addr: requestJson.location.addr
+        }
+      }
+    })
+  );
+
+  res.json({ success: 'post call succeed!', url: req.url, data: requestJson });
 });
 
 /****************************
-* Example post method *
-****************************/
-
-app.post('/blogs', function(req, res) {
+ * Example put method *
+ ****************************/
+app.put('/blogs/*', async function(req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
+  const requestJson = JSON.parse(req.body);
+  const response = await dynamo.send(
+    new UpdateCommand(
+      {
+        TableName: tableName,
+        Key: {
+          id: requestJson.id
+        },
+        UpdateExpression: 'set title = :t, content = :c, description = :d, author = :a, createdTime = :ct, updatedTime = :ut, location = :l',
+        ExpressionAttributeValues: {
+          ':t': requestJson.title,
+          ':c': requestJson.content,
+          ':d': requestJson.description,
+          ':a': requestJson.author,
+          ':ct': requestJson.createdTime,
+          ':ut': requestJson.updatedTime,
+          ':l': {
+            lat: requestJson.location.lat,
+            lng: requestJson.location.lng,
+            addr: requestJson.location.addr
+          }
+        },
+        ReturnValues: 'UPDATED_NEW'
+      }
+    )
+  );
 
-app.post('/blogs/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({ success: 'put call succeed!', url: req.url, data: requestJson, response: response });
 });
 
 /****************************
-* Example put method *
-****************************/
-
-app.put('/blogs', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-app.put('/blogs/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example delete method *
-****************************/
-
-app.delete('/blogs', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.delete('/blogs/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+ * Example delete method *
+ ****************************/
+app.delete('/blogs/*', async function(req, res) {
+  await dynamo.send(
+    new DeleteCommand({
+      TableName: tableName,
+      Key: {
+        id: req.params.id
+      }
+    })
+  );
+  res.json({ success: 'delete call succeed!', url: req.url });
 });
 
 app.listen(3000, function() {
-    console.log("App started")
+  console.log('App started');
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
